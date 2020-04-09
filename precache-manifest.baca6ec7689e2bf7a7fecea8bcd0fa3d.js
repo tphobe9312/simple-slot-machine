a423280c96c65e32490a71c77bbb4ad1cfe77086
amardeep Singh <insphere.amardeep@gmail.com>
2020-04-09 22:51:54 +0530

    first commit

---
+++ precache-manifest.baca6ec7689e2bf7a7fecea8bcd0fa3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb0a8eef225b03859eb97a98583badd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35d26e947226f845f80",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7c0cf4e3c7683fca6cc2",
+    "url": "/static/css/main.ea5f45f4.chunk.css"
+  },
+  {
+    "revision": "f35d26e947226f845f80",
+    "url": "/static/js/2.0fed8e9c.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.0fed8e9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c0cf4e3c7683fca6cc2",
+    "url": "/static/js/main.ad15ecfa.chunk.js"
+  },
+  {
+    "revision": "d3983d5216066d39df67",
+    "url": "/static/js/runtime-main.b87ddbcb.js"
+  }
+]);
